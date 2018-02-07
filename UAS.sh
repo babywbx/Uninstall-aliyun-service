12cdd6d92850c2291cbf3dcbdfe2a2b26fa1e7af
wbx123 <wbx12345678910@gmail.com>
2018-02-07 01:04:08 -0800

    Update UAS.sh

--- UAS.sh
+++ UAS.sh
@@ -62,6 +62,10 @@ iptables -I INPUT -s 140.205.225.206/32 -j DROP
 iptables -I INPUT -s 140.205.225.205/32 -j DROP
 iptables -I INPUT -s 140.205.225.195/32 -j DROP
 iptables -I INPUT -s 140.205.225.204/32 -j DROP
+iptables -I INPUT -s 106.11.222.0/23 -j DROP
+iptables -I INPUT -s 106.11.224.0/24 -j DROP
+iptables -I INPUT -s 106.11.228.0/22 -j DROP
+service iptables save
 cd -
 rm -rf Uninstall-aliyun-service
 
